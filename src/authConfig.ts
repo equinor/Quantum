6615bd0524bd0602ec76fff6d7702ac7eee33128
Jone <joue@equinor.com>
2024-08-26 13:22:02 +0200

    auth

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -21,7 +21,7 @@ export const msalConfig = {
     clientId: "c4c87187-cfeb-40a5-8b72-a87ca9e992e2",
     authority:
       "https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0",
-    redirectUri: "http://localhost:5173",
+    redirectUri: window.location.origin,
   },
   cache: {
     cacheLocation: "sessionStorage", // This configures where your cache will be stored
