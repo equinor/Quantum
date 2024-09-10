377551f70eefbcbf32884d390613ccb13b761cd6
Jone Ueland <joue@equinor.com>
2024-09-10 15:51:30 +0200

    button

--- src/pages/reports/ReportMenu.tsx
+++ src/pages/reports/ReportMenu.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button, Stack } from "react-bootstrap";
 
 const ReportMenu: React.FC = () => {
+  async function printReport() {
+    console.log("print report");
+  }
+
   return (
     <div className="report-menu">
-      <h2>Managment</h2>
+      <Stack direction="horizontal" gap={3}>
+        <h2>Managment</h2>
+        <Button variant="outline-light" onClick={printReport}>
+          Print
+        </Button>
+      </Stack>
     </div>
   );
 };
