e2e8f479016f64d6eaaa6e208da73971dddc961f
Jone <joue@equinor.com>
2024-09-12 14:57:48 +0200

    a

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,8 @@ import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
 import { useIsAuthenticated } from "@azure/msal-react";
 import Button from "react-bootstrap/esm/Button";
+import { SystemData } from "../pages/system/SystemData";
+import { useRequestGraphQL } from "../graphql/GetGraphQL";
 
 /**
  * Renders a drop down button with child buttons for logging in with a popup or redirect
@@ -11,11 +13,28 @@ import Button from "react-bootstrap/esm/Button";
 export const Login = () => {
   const { instance } = useMsal();
   const isAuthenticated = useIsAuthenticated();
+  const { RequestGraphQL } = useRequestGraphQL();
+
+  const fetchCommpkgData = () => {
+    const query = `query GetSystem(){
+      systems {
+        items {
+          SystemId
+          SystemNo
+        }
+      }
+    }`;
+
+    RequestGraphQL<SystemData>(query, {}, (data: SystemData) => {
+      console.log(data);
+    });
+  };
 
   const handleLogin = () => {
     instance.loginPopup(loginRequest).catch((e) => {
       console.log(e);
     });
+    fetchCommpkgData;
   };
 
   const handleLogout = () => {
